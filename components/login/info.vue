d85657a6f92638edd6461c83a9c8a819b7a27ce0
sampson762 <1769301207@qq.com>
2024-11-18 15:44:04 +0800

    perf: 修改登录页

--- components/login/info.vue
+++ components/login/info.vue
@@ -1,20 +1,147 @@
 <script setup lang="ts">
+import type { Rules } from 'common-form'
 
+const props = defineProps<{
+  imageCaptcha: ImageCaptcha
+}>()
+
+const emits = defineEmits<{
+  submit: [val: AccountReq]
+  getCode: []
+}>()
+
+const form = defineModel<AccountReq>({ default: {
+  phone: '',
+  password: '',
+  captcha: '',
+  captcha_id: '',
+} })
+
+const rules = ref<Rules<AccountReq>>({
+  phone: [
+    {
+      message: '手机号不能为空',
+      validator: 'required',
+    },
+    {
+      message: '手机号格式不正确',
+      validator: 'phone',
+      callback: () => {
+        emits('getCode')
+      },
+    },
+  ],
+  password: [
+    {
+      message: '密码不能为空',
+      validator: 'required',
+    },
+  ],
+  captcha: [
+    {
+      message: '验证码不能为空',
+      validator: 'required',
+    },
+    {
+      message: '验证码长度不正确',
+      // 限制验证码长度为 5 个数字
+      validator: val => val?.length === 5,
+      // 验证通过的回调函数，用于处理输入框的值，去除非数字字符
+      callback: (val) => {
+        form.value.captcha = val.replace(/\D/g, '')
+      },
+    },
+  ],
+})
+
+// 登录
 </script>
 
 <template>
-  <div class="cardbox blur-bgc rounded-[16px]">
-    <slot name="form" />
-
-    <div class="px-[20px] mt-[112px] flex-center-row">
-      <div class="h-[1px] w-[58px] color-[#9E9E9E] dark:color-[#fff] bg-gradient-linear-[90deg,#CCCCCC00,#CCCCCC]" />
-      <div class="color-[#9E9E9E] dark:color-[#fff] px-[9px] text-[12px]">
-        其他登录方式
+  <common-form
+    v-model="form" :rules="rules" @submit="(val) => emits('submit', val)">
+    <template
+      #phone="{
+        error,
+        validate,
+      }">
+      <div class="pb-[32px]">
+        <div class="pb-[8px] text-[14px]">
+          手机号
+        </div>
+        <div class="">
+          <input
+            v-model="form.phone" class="input " type="text" placeholder="请输入手机号" :maxlength="11" @change="validate()">
+          <div class="error">
+            {{ error }}
+          </div>
+        </div>
       </div>
-      <div class="h-[1px] w-[58px]  color-[#9E9E9E] dark:color-[#fff] bg-gradient-linear-[270deg,#CCCCCC00,#CCCCCC] " />
-    </div>
-    <div class="flex-center-row py-[24px]">
-      <slot name="other_logins" />
-    </div>
-  </div>
+    </template>
+    <template
+      #password="{
+        error,
+        validate }">
+      <div class="pb-[32px]">
+        <div class="pb-[8px]  text-[14px]">
+          密码
+        </div>
+        <div class="">
+          <input
+            v-model="form.password"
+            class="input "
+            type="password" placeholder="请输入密码" @change="validate()" @blur="validate()">
+          <div class="error">
+            {{ error }}
+          </div>
+        </div>
+      </div>
+    </template>
+    <template
+      v-if="props.imageCaptcha.code"
+      #captcha="{
+        error,
+        validate }">
+      <div class="pb-[32px]">
+        <div class="pb-[8px]  text-[14px]">
+          验证码
+        </div>
+        <div class="relative">
+          <input
+            v-model="form.captcha"
+            :maxlength="5"
+            class="input"
+            type="text" placeholder="请输入验证码"
+            @change="validate()">
+          <div class="absolute right-0 top-0 h-full" @click="emits('getCode')">
+            <img :src="props.imageCaptcha.code" class="h-[100%] rounded-r-[8px]">
+          </div>
+        </div>
+        <div class="error">
+          {{ error }}
+        </div>
+      </div>
+    </template>
+    <template #actions="{ submit }">
+      <div
+        class="text-size-[16px] font-semibold pb-[112px]" @click="submit">
+        <div class="ok">
+          登录
+        </div>
+      </div>
+    </template>
+  </common-form>
 </template>
+
+<style lang="scss" scoped>
+.error {
+  --uno: 'color-[red] text-size-[14px] line-height-[20px] mt-10px';
+}
+.input {
+  --uno: 'px-[12px] py-[10px] bg-[#fff] rounded-[8px] border-0 placeholder-text-[#cbcdd1] text-[14px] w-full outline-none';
+}
+
+.ok {
+  --uno: 'bg-gradient-linear-[180deg,#1A6BEB,#6EA6FF] line-height-[24px] px-[77px] py-[6px] text-center rounded-[36px] color-[#fff] shadow-[0_8px_8px_0px_#3971F33D]';
+}
+</style>
---
+++ components/login/other.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+const emits = defineEmits<{
+  other: [val: string]
+}>()
+
+const otherList = ref([{
+  name: 'wxwork',
+  icon: 'i-svg:qwicon',
+}])
+</script>
+
+<template>
+  <div>
+    <div class="px-[20px]  flex-center-row">
+      <div class="h-[1px] w-[58px] color-[#9E9E9E] dark:color-[#fff] bg-gradient-linear-[90deg,#CCCCCC00,#CCCCCC]" />
+      <div class="color-[#9E9E9E] dark:color-[#fff] px-[9px] text-[12px]">
+        其他登录方式
+      </div>
+      <div class="h-[1px] w-[58px]  color-[#9E9E9E] dark:color-[#fff] bg-gradient-linear-[270deg,#CCCCCC00,#CCCCCC] " />
+    </div>
+    <template v-for="(item, index) in otherList" :key="index">
+      <div class="flex-center-row py-[24px]">
+        <div class="wh-[32px] rounded-full flex-center-row" @click="emits('other', item.name)">
+          <icon :name="item.icon" size="32" />
+        </div>
+      </div>
+    </template>
+  </div>
+</template>
