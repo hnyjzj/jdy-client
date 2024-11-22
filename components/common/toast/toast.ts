4eac8f1f00b2fff55436510cb6214560243bb0af
waset <waset@foxmail.com>
2024-11-22 17:30:05 +0800

    refactor: 优化toast组件结构及类型定义

---
+++ components/common/toast/toast.ts
@@ -0,0 +1,105 @@
+import type { Toast as ToastOptions } from './type'
+import { createVNode, type CSSProperties, render } from 'vue'
+import Template from './Index.vue'
+
+export default class Toast {
+  ROOTDOM_ID = '__toast__'
+
+  private rootDom = document.getElementById(this.ROOTDOM_ID)
+
+  constructor(options?: ToastOptions) {
+    this.createRootDom()
+    if (options) {
+      this.show(options)
+    }
+  }
+
+  show(options: ToastOptions) {
+    const dom = this.createDom()
+    this.createNode(dom, options)
+
+    setTimeout(() => {
+      dom.remove()
+    }, options.duration || 3000)
+
+    // 向前插入
+    this.rootDom?.insertBefore(dom, this.rootDom.firstChild)
+  }
+
+  success = (message: string, duration?: number) => {
+    this.show({ message, duration, type: 'success' })
+  }
+
+  error = (message: string, duration?: number) => {
+    this.show({ message, duration, type: 'error' })
+  }
+
+  warning = (message: string, duration?: number) => {
+    this.show({ message, duration, type: 'warning' })
+  }
+
+  primary = (message: string, duration?: number) => {
+    this.show({ message, duration, type: 'primary' })
+  }
+
+  default = (message: string, duration?: number) => {
+    this.show({ message, duration, type: 'primary' })
+  }
+
+  closeAll() {
+    const toasts = this.rootDom?.querySelectorAll('div')
+    if (toasts) {
+      toasts.forEach((toast) => {
+        toast.remove()
+      })
+    }
+  }
+
+  private createRootDom() {
+    const style: CSSProperties = {
+      position: 'fixed',
+      zIndex: '9999',
+      top: '8px',
+      left: '0',
+      right: '0',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      pointerEvents: 'none',
+      userSelect: 'none',
+      maxHeight: '50vh',
+      overflow: 'hidden',
+    }
+
+    if (!this.rootDom) {
+      this.rootDom = document.createElement('div')
+      this.rootDom.id = this.ROOTDOM_ID
+      Object.assign(this.rootDom.style, style)
+
+      document.body.appendChild(this.rootDom)
+    }
+  }
+
+  private createDom() {
+    const toast = document.createElement('div')
+    const style: CSSProperties = {
+      width: '100%',
+      paddingLeft: '2vw',
+      paddingRight: '2vw',
+      marginBottom: '8px',
+      transition: 'all 0.3s ease-in-out',
+    }
+
+    Object.assign(toast.style, style)
+
+    return toast
+  }
+
+  private createNode(dom: HTMLElement, props: ToastOptions & any) {
+    const vm = createVNode(Template, props)
+    render(vm, dom)
+
+    return vm
+  }
+}
--- components/sale/add/Settlement.vue
+++ components/sale/add/Settlement.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-import { useToast } from '~/composables/toast'
-
 const props = defineProps<{
   goodinfo: any[]
 }>()
@@ -15,10 +13,16 @@ const getInitialItems = () => [{ id: 1, isPopoverVisible: false, actions, select
 const items = ref(getInitialItems())
 let id = items.value.length + 1
 
+const { $toast } = useNuxtApp()
+
 const insertItem = () => {
   if (items.value.length >= 3) {
-    useToast().show({ message: '最多只能添加三个支付信息', icon: 'i-icon:warning', duration: 2000 })
-    useToast().show({ message: '最多只能添', icon: 'i-icon:warning', duration: 3000 })
+    $toast.success('11111111')
+    $toast.primary('22222222')
+    $toast.error('333333333')
+    $toast.warning('44444444444')
+    $toast.default('44444444444')
+    $toast.show({ message: '11111111' })
     return
   }
   // 添加新的支付信息栏并初始化状态
@@ -88,11 +92,7 @@ function removeItem(item: { id: number }) {
                 :actions="item.actions"
                 @select="(action) => {
                   if (items.some(i => i !== item && i.selected === action.text)) {
-                    $toast({
-                      message: '该支付方式已被选择',
-                      theme: 'warning',
-                      icon: 'i-icon:warning',
-                    })
+                    $toast.warning('该支付方式已被选择')
                     return
                   }
                   item.selected = action.text
