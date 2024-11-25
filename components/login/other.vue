8eec7c800c97eb4eb4290f89f96d2c804e22f01b
sampson762 <1769301207@qq.com>
2024-11-25 15:51:08 +0800

    chore: 修改文件名

--- components/login/other.vue
+++ components/login/other.vue
@@ -12,9 +12,7 @@ const emits = defineEmits<{
   <div>
     <div class="px-[20px]  flex-center-row">
       <div class="h-[1px] w-[58px] color-[#9E9E9E] dark:color-[#fff] bg-gradient-linear-[90deg,#CCCCCC00,#CCCCCC]" />
-      <div class="color-[#9E9E9E] dark:color-[#fff] px-[9px] text-[12px]">
-        其他登录方式
-      </div>
+      <div class="color-[#9E9E9E] dark:color-[#fff] px-[9px] text-[12px]" />
       <div class="h-[1px] w-[58px]  color-[#9E9E9E] dark:color-[#fff] bg-gradient-linear-[270deg,#CCCCCC00,#CCCCCC] " />
     </div>
     <template v-for="(item, index) in props.list" :key="index">
