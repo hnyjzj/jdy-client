9d1105723fb0fbfefa82e1be276326335d603553
sampson762 <1769301207@qq.com>
2024-11-27 14:11:38 +0800

    fix: 修改默认值

--- components/login/Other.vue
+++ components/login/Other.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup>
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   list: { name: string, icon: string }[]
   text?: string
-}>()
+}>(), {
+  text: '其他登录方式',
+})
 
 const emits = defineEmits<{
   other: [val: string]
@@ -14,7 +16,7 @@ const emits = defineEmits<{
     <div class="px-[20px]  flex-center-row">
       <div class="h-[1px] w-[58px] color-[#9E9E9E] dark:color-[#fff] bg-gradient-linear-[90deg,#CCCCCC00,#CCCCCC]" />
       <div class="color-[#9E9E9E] dark:color-[#fff] px-[9px] text-[12px]">
-        {{ props.text ? props.text : '其他登录方式' }}
+        {{ props.text }}
       </div>
       <div class="h-[1px] w-[58px]  color-[#9E9E9E] dark:color-[#fff] bg-gradient-linear-[270deg,#CCCCCC00,#CCCCCC] " />
     </div>
