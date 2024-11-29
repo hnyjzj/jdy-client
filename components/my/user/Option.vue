7a7d72cc431ec772b79e2af27ef60baa39007524
sampson762 <1769301207@qq.com>
2024-11-29 15:31:10 +0800

    fix: 调整选项卡片高度

--- components/my/user/Option.vue
+++ components/my/user/Option.vue
@@ -1,41 +1,26 @@
+<script setup lang="ts">
+const props = defineProps<{
+  optList: UserOpts[]
+}>()
+</script>
+
 <template>
-  <div class="py-[14px] px-[16px] blur-bgc rounded-[16px]">
-    <div class="">
-      <nuxt-link to="/my/user/information" class="color-[#000]">
-        <div class="flex-between">
-          <div class="flex-center-row">
-            <icon name="i-svg:my-profile" size="32" />
-            <div class="font-semibold text-[14px] line-height-[20px] ml-[8px] dark:color-[#fff]">
-              资料管理
+  <div class=" px-[16px] blur-bgc rounded-[16px]">
+    <template v-for="(item, index) in props.optList" :key="index">
+      <div class="cursor-pointer">
+        <nuxt-link :to="item.link" class="color-[#000]">
+          <div class="flex-between">
+            <div class="flex-center-row py-[24px]">
+              <icon :name="item.iconName" size="32" />
+              <div class="font-semibold text-[14px] line-height-[20px] ml-[8px] dark:color-[#fff]">
+                {{ item.optName }}
+              </div>
             </div>
+            <van-icon name="arrow" size="20px" class="color-[#808089]" />
           </div>
-          <van-icon name="arrow" size="20px" class="color-[#808089]" />
-        </div>
-        <div class="mb-[24px] mt-[12px] w-full h-[1px] bg-[#E6E6E8]" />
-      </nuxt-link>
-    </div>
-    <div class="">
-      <div class="flex-between">
-        <div class="flex-center-row">
-          <icon name="i-svg:my-setting" size="32" />
-          <div class="font-semibold text-[14px] line-height-[20px]  ml-[8px] dark:color-[#fff]">
-            提醒设置
-          </div>
-        </div>
-        <van-icon name="arrow" size="20px" class="color-[#808089]" />
+          <div class="w-full h-[1px] bg-[#E6E6E8] dark:opacity-[0.5]" :style="{ background: index !== props.optList.length - 1 ? '#E6E6E8' : 'none' }" />
+        </nuxt-link>
       </div>
-      <div class="mb-[24px] mt-[12px] w-full h-[1px] bg-[#E6E6E8]" />
-    </div>
-    <div class="">
-      <div class="flex-between">
-        <div class="flex-center-row">
-          <icon name="i-svg:my-question" size="32" />
-          <div class="font-semibold text-[14px] line-height-[20px]  ml-[8px] dark:color-[#fff]">
-            问题反馈
-          </div>
-        </div>
-        <van-icon name="arrow" size="20px" class="color-[#808089]" />
-      </div>
-    </div>
+    </template>
   </div>
 </template>
