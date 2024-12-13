264196a4f95b48515ec619daac6ff7d98ccc94a9
LdddLi <1653243099@qq.com>
2024-12-13 17:47:00 +0800

    feat: 新增短信历史记录页

---
+++ pages/member/lists/historia.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="grid-12">
+    <div class="flex flex-col gap-[16px] px-[16px] py-[16px] col-12" uno-lg="col-8 offset-2">
+      <div class="flex flex-row items-center justify-between gap-[32px]">
+        <product-filter-search class="color-[#fff] flex-1" />
+        <product-filter-senior class="color-[#fff]" />
+      </div>
+
+      <member-lists-historia />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
