2a74ac7c44e418d86767ffcc2dcbba6a5e8c56ff
XieJingao <496929151@qq.com>
2024-12-16 15:19:20 +0800

    refactor: 使用为gradient组件

--- components/product/finished/List.vue
+++ components/product/finished/List.vue
@@ -14,46 +14,44 @@ const goodsStatus = {
 </script>
 
 <template>
-  <div class="grid-12 px-[16px] mb-12">
-    <div class="w-auto bg-white blur-bga rounded-[24px] border-solid border-[#EFF0F6] col-12 overflow-hidden" uno-lg="col-8 offset-2" uno-md="col-12">
-      <div class="rounded-[24px]">
-        <common-header-theme :title="props.productInfo.name ?? ''">
-          <common-tags type="orange" :text="goodsStatus[props.productInfo.status] ?? ''" />
-        </common-header-theme>
-        <div class="flex flex-col gap-3 px-4 py-3">
-          <template v-for="(item, index) in props.filterListToArray" :key="index">
-            <template v-if="item.label">
-              <div class="flex-center-between text-sm font-normal even:bg-[rgba(215,215,215,0.2)]">
-                <div class="text-color-light">
-                  {{ item?.label }}
-                </div>
-                <div class="text-color">
-                  <template v-if="item.input === 'select'">
-                    <span>
-                      {{ filterList[item.name]?.preset[props.productInfo[item.name]] ?? '' }}
-                    </span>
-                  </template>
-                  <template v-else-if="item.input === 'text'">
-                    <span>
-                      {{ props.productInfo[item.name] ?? '' }}
-                    </span>
-                  </template>
-                  <template v-else-if="item.input === 'number'">
-                    <span>
-                      {{ props.productInfo[item.name] ?? '' }}
-                    </span>
-                  </template>
-                  <template v-else-if="item.input === 'switch'">
-                    <span>
-                      {{ props.productInfo[item.name] ? '是' : '否' }}
-                    </span>
-                  </template>
-                </div>
+  <common-gradient theme="theme" :title="props.productInfo.name ?? ''">
+    <template #right>
+      <common-tags type="orange" :text="goodsStatus[props.productInfo.status] ?? ''" />
+    </template>
+    <template #body>
+      <div class="flex flex-col gap-3 px-4 py-3">
+        <template v-for="(item, index) in props.filterListToArray" :key="index">
+          <template v-if="item.label">
+            <div class="flex-center-between text-sm font-normal even:bg-[rgba(215,215,215,0.2)]">
+              <div class="text-color-light">
+                {{ item?.label }}
               </div>
-            </template>
+              <div class="text-color">
+                <template v-if="item.input === 'select'">
+                  <span>
+                    {{ filterList[item.name]?.preset[props.productInfo[item.name]] ?? '' }}
+                  </span>
+                </template>
+                <template v-else-if="item.input === 'text'">
+                  <span>
+                    {{ props.productInfo[item.name] ?? '' }}
+                  </span>
+                </template>
+                <template v-else-if="item.input === 'number'">
+                  <span>
+                    {{ props.productInfo[item.name] ?? '' }}
+                  </span>
+                </template>
+                <template v-else-if="item.input === 'switch'">
+                  <span>
+                    {{ props.productInfo[item.name] ? '是' : '否' }}
+                  </span>
+                </template>
+              </div>
+            </div>
           </template>
-        </div>
+        </template>
       </div>
-    </div>
-  </div>
+    </template>
+  </common-gradient>
 </template>
