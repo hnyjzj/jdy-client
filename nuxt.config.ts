40cef9247e8071b1712b63cfe7b2870cb8e873b8
waset <waset@foxmail.com>
2024-12-18 16:09:59 +0800

    feat: 添加ecosystem配置，禁用telemetry

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-06',
+  telemetry: false,
   devtools: {
     enabled: true,
   },
