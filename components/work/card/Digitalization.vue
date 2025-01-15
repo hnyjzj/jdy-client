93e6856e76af4a50b586ac8f1880ee86c478ccb1
sampson762 <1769301207@qq.com>
2025-01-15 16:44:10 +0800

    feat: 增加首页今日销售滑动

--- components/work/card/Digitalization.vue
+++ components/work/card/Digitalization.vue
@@ -5,6 +5,14 @@ const props = withDefaults(defineProps<{
 }>(), {
   marginBottom: '16px',
 })
+
+const { run, stop } = addMouseEvent(`#today`)
+onMounted(() => {
+  run()
+})
+onBeforeUnmount(() => {
+  stop()
+})
 </script>
 
 <template>
@@ -19,46 +27,48 @@ const props = withDefaults(defineProps<{
       </div>
     </div>
 
-    <div class="pt-[10px] pl-[6px] text-[10px]" uno-sm="text-[14px]">
-      <div
-        class="bg-[rgba(199,218,255,0.8)] py-[5px] pl-[6px] pr-[10px] rounded-l-[20px]  grid-12">
-        <div class="items-title">
-          门店
+    <div class="overflow-hidden">
+      <div id="today" class="pt-[10px] pl-[6px] text-[10px] overflow-x-scroll" uno-sm="text-[14px]">
+        <div
+          class="flex">
+          <div class="items-title rounded-l-[40px]">
+            门店
+          </div>
+          <div class="items-title">
+            总业绩
+          </div>
+          <div class="items-title">
+            中回收旧料抵扣
+          </div>
+          <div class="items-title">
+            中回收旧料抵扣
+          </div>
+          <div class="items-title">
+            低毛利
+          </div>
+          <div class="items-title">
+            低销售额
+          </div>
         </div>
-        <div class="items-title">
-          总业绩
-        </div>
-        <div class="items-title">
-          中回收旧料抵扣
-        </div>
-        <div class="items-title">
-          中回收旧料抵扣
-        </div>
-        <div class="items-title">
-          低毛利
-        </div>
-        <div class="items-title">
-          低销售额
-        </div>
-      </div>
-      <div class="py-[12px] pb-[16px] pl-[6px] pr-[10px] rounded-l-[20px] text-[10px] grid-12 " uno-sm="text-[14px]">
-        <div class="items">
-          平顶山潮宏基万达店
-        </div>
-        <div class="items">
-          123344
-        </div>
-        <div class="items">
-          123344
-        </div>
-        <div class="items">
-          123344
-        </div>
-        <div class="items">
-          123344
-        </div>
-        <div class="items">
-          123344
+        <div class="py-[12px] pb-[16px] pl-[6px] pr-[10px] rounded-l-[20px] text-[10px] flex" uno-sm="text-[14px]">
+          <div class="items">
+            平顶山潮宏基万达店
+          </div>
+          <div class="items">
+            123344
+          </div>
+          <div class="items">
+            123344
+          </div>
+          <div class="items">
+            123344
+          </div>
+          <div class="items">
+            123344
+          </div>
+          <div class="items">
+            123344
+          </div>
         </div>
       </div>
     </div>
@@ -67,9 +77,9 @@ const props = withDefaults(defineProps<{
 
 <style lang="scss" scoped>
 .items-title {
-  --uno: 'col-2 flex-center-row text-center color-#4B576D';
+  --uno: 'col-2 flex-center-row text-center color-#4B576D flex-grow-1 flex-shrink-0 w-[100px] px-[10px] py-[6px] bg-[rgba(199,218,255,0.8)] ';
 }
 .items {
-  --uno: 'col-2 flex-center-row text-center color-[#3971F3] dark:color-[#fff]';
+  --uno: 'col-2 flex-center-row text-center color-[#3971F3] dark:color-[#fff] flex-grow-1 flex-shrink-0 w-100px px-10px';
 }
 </style>
