6f59da2a071a6b80ad4f08cee00efe09ef51987e
sampson762 <1769301207@qq.com>
2025-01-25 14:32:47 +0800

    feat: 注释验证成功提示信息 (#118)

--- components/staff/manage/Add.vue
+++ components/staff/manage/Add.vue
@@ -51,7 +51,7 @@ function handleValidateButtonClick(e: MouseEvent) {
   e.preventDefault()
   formRef.value?.validate((errors: any) => {
     if (!errors) {
-      message.success('验证成功')
+    //   message.success('验证成功')
       emits('submit')
     }
     else {
