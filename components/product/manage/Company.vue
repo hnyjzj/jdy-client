512a5ff032426283110248125b67662a9e18fdd6
sampson762 <1769301207@qq.com>
2025-02-10 16:20:42 +0800

    fix: 优化门店选择样式及响应式宽度

--- components/product/manage/Company.vue
+++ components/product/manage/Company.vue
@@ -25,7 +25,10 @@ function handleSelect(id: Stores['id']) {
 <template>
   <div>
     <n-dropdown trigger="click" placement="bottom-start" :options="columns" @select="handleSelect">
-      <div class="py-[6px] px-[12px] bg-[#FFFFFF66] border-rd-full h-full flex-center-row shadow-lg cursor-pointer" @click="changeStoer">
+      <div
+        class="py-[6px] px-[12px] w-[150px] bg-[#FFFFFF66] border-rd-full h-full flex-center-row shadow-lg cursor-pointer "
+        uno-sm="w-[200px]"
+        @click="changeStoer">
         <div class="store-name font-bold text-size-[14px] mr-[4px]">
           {{ myStore.name || '选择门店' }}
         </div>
