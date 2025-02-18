12f2ee4af8ef564860e7155f2f53b32601436e84
sampson762 <1769301207@qq.com>
2025-02-18 15:50:15 +0800

    style: 优化员工新增页面布局及样式

--- components/staff/manage/Add.vue
+++ components/staff/manage/Add.vue
@@ -17,7 +17,7 @@ const formlist = defineModel({ default: {
     gender: 0,
   },
 } })
-
+const genderList = [{ text: '未知', value: 0 }, { text: '男', value: 1 }, { text: '女', value: 2 }]
 const rules = {
   phone: {
     required: true,
@@ -90,7 +90,7 @@ defineExpose({
 
 <template>
   <div>
-    <div class="pb-[16px]">
+    <div class="pb-[70px]">
       <common-fold title="新增员工" from-color="#9EBAF9" to-color="#fff">
         <div class="p-[16px]">
           <n-form
@@ -98,6 +98,7 @@ defineExpose({
             :model="formlist.account"
             :rules="rules"
             label-placement="top"
+            size="large"
           >
             <n-form-item :span="12" label="手机号" path="phone">
               <n-input
@@ -130,15 +131,16 @@ defineExpose({
             <n-form-item :span="12" label="性别" path="gender">
               <n-radio-group v-model:value="formlist.account.gender">
                 <n-space>
-                  <n-radio :value="0">
-                    未知
-                  </n-radio>
-                  <n-radio :value="1">
-                    男
-                  </n-radio>
-                  <n-radio :value="2">
-                    女
-                  </n-radio>
+                  <template v-for="(item, index) in genderList" :key="index">
+                    <n-radio
+                      :value="item.value" :style="{
+                        '--n-box-shadow-hover': 'inset 0 0 0 1px #0068ff',
+                        '--n-box-shadow-active': 'inset 0 0 0 1px #0068ff',
+                        '--n-dot-color-active': '#0068ff',
+                        '--n-box-shadow-focus': 'inset 0 0 0 1px #0068ff, 0 0 0 2px rgba(24, 65, 160, 0.2)' }">
+                      {{ item.text }}
+                    </n-radio>
+                  </template>
                 </n-space>
               </n-radio-group>
             </n-form-item>
@@ -146,19 +148,23 @@ defineExpose({
               label="头像">
               <n-upload
                 :key="keyUpload"
+                :style="{ '--n-dragger-border-hover': '#0068ff' }"
                 action="#"
                 :custom-request="customRequest"
                 :default-file-list="previewFileList"
                 list-type="image-card"
                 :max="1"
+
                 @before-upload="beforeUpload"
                 @preview="showModalRef = true"
               />
             </n-form-item>
-            <div
-              class="text-size-[16px] font-semibold py-[16px] cursor-pointer" @click="handleValidateButtonClick">
-              <div class="ok">
-                确定
+            <div class="fixed bottom-0 left-0 bg-[#fff] w-full grid-12 px-[26px]">
+              <div
+                class="text-size-[16px] font-semibold pt-[16px] pb-[26px] cursor-pointer col-12" uno-sm="col-8 offset-2" uno-lg="col-4 offset-4" @click="handleValidateButtonClick">
+                <div class="ok">
+                  确定
+                </div>
               </div>
             </div>
           </n-form>
@@ -184,5 +190,9 @@ defineExpose({
 }
 .label {
   --uno: 'pb-[8px] dark:color-#fff';
+}
+
+.n-radio .n-radio__dot.n-radio__dot--checked {
+  box-shadow: inset 0 0 0 1px #0068ff;
 }
 </style>
