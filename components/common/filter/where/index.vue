c7fed2beef635d25e2ae2afb0ff2cc161dbfeb62
XieJingao <496929151@qq.com>
2025-02-18 18:00:53 +0800

    fix: 筛选组件日期格式代码冲突问题

--- components/common/filter/where/index.vue
+++ components/common/filter/where/index.vue
@@ -127,7 +127,7 @@ const presetToSelect = (filter: FilterWhere<T>): { label: string, value: any }[]
                     <n-input v-model:value="datas[name as string]" :placeholder="`输入${label}`" type="textarea" maxlength="255" round :autosize="{ minRows: 2, maxRows: 3 }" :disabled="datas?.disabled?.includes(name)" />
                   </template>
                   <template v-if="input === 'date'">
-                    <n-date-picker v-model:value="datas[name as string]" value-format="yyyy.MM.dd HH:mm:ss" type="datetime" size="large" :placeholder="`选择${label}`" round clearable :disabled="datas?.disabled?.includes(name)" />
+                    <n-date-picker v-model:value="datas[name as string]" value-format="yyyy-MM-dd'T'HH:mm:ss.SSSxxx" type="datetime" size="large" :placeholder="`选择${label}`" round clearable :disabled="datas?.disabled?.includes(name)" />
                   </template>
                 </slot>
               </div>
