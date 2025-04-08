9210141f2500fc5323b9c962764147d8d269a35b
XieJingao <496929151@qq.com>
2025-04-08 17:56:07 +0800

    feat: 配件记录及详情页

---
+++ components/accessorie/record/table.vue
@@ -0,0 +1,88 @@
+<script lang="ts" setup>
+const props = withDefaults(defineProps<{
+  oldAccessories: ProductAccessories
+  newAccessories: ProductAccessories
+  filterList: FilterWhere<AccessorieCategory>[]
+}>(), {
+})
+
+type ProductKey = keyof AccessorieCategory
+/** 字段是否更新 */
+function isUpdate(key: ProductKey) {
+  try {
+    return JSON.stringify(props.oldAccessories.category[key]) !== JSON.stringify(props.newAccessories.category[key])
+  }
+  catch {
+    return false
+  }
+}
+</script>
+
+<template>
+  <div class="info-row">
+    <div class="info-title">
+      库存（后/前）
+    </div>
+    <div class="info-val" :style="props.oldAccessories?.stock !== props.newAccessories?.stock ? 'color: #FF4D4F' : ''">
+      {{ props.newAccessories.stock }}/{{ props.oldAccessories.stock }}
+    </div>
+  </div>
+  <div class="info-row">
+    <div class="info-title">
+      变换数量
+    </div>
+    <div class="info-val">
+      {{ props.oldAccessories.stock && props.newAccessories.stock ? props.newAccessories.stock - props.oldAccessories.stock : '' }}
+    </div>
+  </div>
+  <div class="info-row" :style="props.oldAccessories?.store_id === props.newAccessories?.store_id ? 'color: #FF4D4F' : ''">
+    <div class="info-title">
+      所属门店
+    </div>
+    <div class="info-val">
+      {{ props.newAccessories?.store?.name }}
+    </div>
+  </div>
+  <template v-for="(item, index) in filterList" :key="index">
+    <template v-if="item.create">
+      <div class="info-row">
+        <div class="info-title">
+          {{ item.label }}
+        </div>
+        <div class="text-color-light" :style="isUpdate(item.name) ? 'color: #FF4D4F' : ''">
+          <template v-if="item.input === 'select'">
+            <div class="info-val">
+              {{ item.preset[props.newAccessories.category[item.name]] }}
+            </div>
+          </template>
+          <template v-else>
+            <div class="info-val">
+              {{ props.newAccessories.category[item.name] }}
+            </div>
+          </template>
+        </div>
+      </div>
+    </template>
+  </template>
+</template>
+
+<style lang="scss" scoped>
+.key {
+  --uno: 'text-size-[14px] color-[#666] dark:color-[#CBCDD1] mr-2';
+}
+.value {
+  --uno: 'text-size-[14px] color-[#333] dark:color-[#fff] w-[60%]';
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+}
+.info-row {
+  --uno: 'flex justify-between mb-2';
+  .info-title {
+    --uno: 'text-color';
+  }
+  .info-val {
+    --uno: 'w-200px text-right text-color-light';
+  }
+}
+</style>
--- pages/product/accessorie/record/index.vue
+++ pages/product/accessorie/record/index.vue
@@ -1,13 +1,139 @@
-<script lang="ts" setup>
+<script setup lang="ts">
+const { $toast } = useNuxtApp()
+const { getProductHistory, getHistoryWhere } = useAcessorieRecord()
+const { historyFilterList, historyListTotal, productRocordList, HistoryFilterListToArray } = storeToRefs(useAcessorieRecord())
+const { storesList, myStore } = storeToRefs(useStores())
+const { getStoreList, getMyStore } = useStores()
+const { categoryFilterListToArray } = storeToRefs(useAccessorieCategory())
+const { getAccessorieCategoryWhere } = useAccessorieCategory()
+const searchKey = ref('')
+const complate = ref(0)
+// 筛选框显示隐藏
+const isFilter = ref(false)
+const pages = ref(1)
+const storeCol = ref()
+function changeStore() {
+  storeCol.value = []
+  storesList.value.forEach((item: Stores) => {
+    storeCol.value.push({ label: item.name, value: item.id })
+  })
+}
 useSeoMeta({
   title: '配件记录',
 })
+
+const openFilter = () => {
+  isFilter.value = true
+}
+/** 搜索 */
+async function search(e: string) {
+  await submitWhere({ product_id: e }, true)
+}
+/** 关闭搜索 */
+async function clearSearch() {
+  await submitWhere({ }, true)
+}
+// 获取货品列表
+async function getList(where = {} as Partial<HistoryWhere>) {
+  const params = { page: pages.value, limit: 10 } as ReqList<HistoryWhere>
+  params.where = where
+  if (myStore.value.id) {
+    where.store_id = myStore.value.id
+    where.type = [3]
+  }
+  const res = await getProductHistory(params)
+  return res as any
+}
+try {
+  await getList()
+  await getHistoryWhere()
+  await getAccessorieCategoryWhere()
+  await changeStore()
+  await getStoreList({ page: 1, limit: 20 })
+  await getMyStore({ page: 1, limit: 20 })
+}
+catch (error) {
+  $toast.error('初始化数据失败')
+  console.error('初始化数据失败:', error)
+}
+
+const filterData = ref({} as Partial<AccessorieRecord>)
+
+function pull() {
+  getList(filterData.value)
+}
+
+// 筛选列表
+async function submitWhere(f: Partial<HistoryWhere>, isSearch: boolean = false) {
+  filterData.value = { ...f }
+  pages.value = 1
+  productRocordList.value = []
+  const res = await getList(filterData.value)
+  if (res.code === HttpCode.SUCCESS) {
+    if (!isSearch) {
+      $toast.success('筛选成功')
+    }
+    return
+  }
+  $toast.error(res.message ?? '筛选失败')
+}
 </script>
 
 <template>
-  <div />
+  <div>
+    <!-- 筛选 -->
+    <product-filter
+      v-model:id="complate" v-model:search="searchKey" :product-list-total="historyListTotal" placeholder="搜素关联产品单号" @filter="openFilter" @search="search" @clear-search="clearSearch">
+      <template #company>
+        <product-manage-company />
+      </template>
+    </product-filter>
+    <!-- 列表 -->
+    <div class="px-[16px] pb-10">
+      <template v-if="productRocordList?.length">
+        <product-manage-card :list="productRocordList">
+          <template #top="{ info }">
+            <div>{{ historyFilterList.action?.preset[info.action] }}</div>
+          </template>
+          <template #info="{ info }">
+            <div class="px-[16px] py-[8px] text-size-[14px] line-height-[20px] text-black dark:text-[#FFF]">
+              <div class="flex-between">
+                <div>
+                  操作时间
+                </div>
+                <div class="text-align-end">
+                  {{ formatTimestampToDateTime(info.updated_at) }}
+                </div>
+              </div>
+              <div class="flex-between">
+                <div>
+                  关联单号
+                </div>
+                <div class="text-align-end">
+                  {{ info.source_id }}
+                </div>
+              </div>
+
+              <accessorie-record-card :filter-list="categoryFilterListToArray" :accessories="info.new_value?.product ? info.new_value?.product : info.new_value" />
+            </div>
+          </template>
+          <template #bottom="{ info }">
+            <div class="flex-end text-size-[14px]">
+              <common-button-irregular text="详情" @click="jump('/product/accessorie/record/info', { id: info?.id })" />
+            </div>
+          </template>
+        </product-manage-card>
+        <common-page
+          v-model:page="pages" :total="historyListTotal" :limit="10" @update:page="() => {
+            pull()
+          }
+          " />
+      </template>
+      <template v-else>
+        <common-empty width="100px" />
+      </template>
+
+      <common-filter-where v-model:show="isFilter" :data="filterData" :filter="HistoryFilterListToArray" @submit="submitWhere" />
+    </div>
+  </div>
 </template>
-
-<style lang="scss" scoped>
-
-</style>
