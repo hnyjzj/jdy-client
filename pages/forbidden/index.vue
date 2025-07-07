9d5960a140bcc0336fb8110ce09807b1980d860a
sampson762 <1769301207@qq.com>
2025-07-07 16:03:18 +0800

    feat: 修改错误显示

--- pages/forbidden/index.vue
+++ pages/forbidden/index.vue
@@ -23,6 +23,10 @@ const jumpTo = () => {
 useHead({
   title: '无权限',
 })
+const displayMessage = computed(() => {
+  const message = route.query.message
+  return (Array.isArray(message) ? message[0] : message) || '您暂无访问权限!'
+})
 </script>
 
 <template>
@@ -34,7 +38,7 @@ useHead({
         </div>
         <div class="flex-center-col gap-[16px]">
           <div class="font-semibold text-[32px] color-[#333] dark:color-[#F2F2F2] row-height">
-            {{ route.query.message }}
+            {{ displayMessage }}
           </div>
           <div class="font-size-[20px] color-[#333] dark:color-[#F2F2F2] flex-center-col gap-[6px] row-height">
             <span>请联系管理员处理。</span>
