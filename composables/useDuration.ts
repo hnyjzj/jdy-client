4027899ca2028e18d43064fa0914288fe919e370
sampson762 <1769301207@qq.com>
2025-07-28 09:40:42 +0800

    feat: 修改首页枚举

---
+++ composables/useDuration.ts
@@ -0,0 +1,35 @@
+/**
+ * Duration，时间区间
+ *
+ * 今天
+ *
+ * 昨天
+ *
+ * 本周
+ *
+ * 上周
+ *
+ * 本月
+ *
+ * 上月
+ *
+ * 本季度
+ *
+ * 上季度
+ *
+ * 今年
+ *
+ * 去年
+ */
+export enum Duration {
+  today = 1,
+  yesterday,
+  week,
+  last_week,
+  month,
+  last_month,
+  quarter,
+  last_quarter,
+  year,
+  last_year,
+}
