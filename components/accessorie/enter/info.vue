654d358d4ac86021314fb21080d9d3bc292add67
glooe <93868134+XieJingao@users.noreply.github.com>
2025-09-03 16:02:31 +0800

    feat: 新增文本省略提示功能 (#264)

--- components/accessorie/enter/info.vue
+++ components/accessorie/enter/info.vue
@@ -44,12 +44,12 @@ function updatePage(e: number) {
                       </div>
                       <template v-if="filter.input === 'select'">
                         <div class="value">
-                          {{ filter.preset[item[filter.name]] }}
+                          <common-tooltip :val="filter.preset[item[filter.name]]" />
                         </div>
                       </template>
                       <template v-else>
                         <div class="value">
-                          {{ item[filter.name] }}
+                          <common-tooltip :val="item[filter.name]" />
                         </div>
                       </template>
                     </div>
---
+++ components/common/tooltip/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = defineProps<{
+  val: any
+}>()
+</script>
+
+<template>
+  <n-tooltip v-if="props.val" placement="bottom" trigger="click">
+    <template #trigger>
+      <div class="ellipsis-value cursor-pointer">
+        {{ props.val }}
+      </div>
+    </template>
+    <span>{{ props.val }}</span>
+  </n-tooltip>
+</template>
+
+<style scoped>
+.ellipsis-value {
+  white-space: nowrap; /* 不换行 */
+  overflow: hidden; /* 隐藏超出 */
+  text-overflow: ellipsis; /* 省略号 */
+}
+</style>
