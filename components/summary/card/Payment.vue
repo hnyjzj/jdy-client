7a5150a16123915a6a6d8e26e5c49fa9c96df6ca
sampson762 <1769301207@qq.com>
2025-09-05 10:19:56 +0800

    feat: 添加今日收支卡片组件及功能
    - 新增Payment.vue组件用于展示今日收支数据
    - 在home store中添加Payments状态和获取方法
    - 在首页集成收支卡片并添加点击标题跳转功能
    - 修改Table和Card组件以支持标题点击事件

---
+++ components/summary/card/Payment.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  rbText?: string
+  marginBottom?: string
+  goldPrice?: string
+  payments: Record<string, string>
+}>(), {
+  rbText: '查看排行',
+  marginBottom: '16px',
+})
+
+const id = useId()
+const { run, stop } = addMouseEvent(`#${id}`)
+onMounted(() => {
+  run()
+})
+onBeforeUnmount(() => {
+  stop()
+})
+</script>
+
+<template>
+  <div class="blur-bgc rounded-[16px]  cursor-pointer" :style="{ marginBottom: props.marginBottom }">
+    <div class="grid-12">
+      <div
+        class="skew col-6" uno-md="col-4">
+        <div class="skew-right" />
+        <div class="skew-text pl-[15px] text-[16px] font-semibold">
+          <div class="flex-center-row h-full">
+            今日收支
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="p-[16px] ">
+      <div :id="id" class="flex overflow-x-scroll gap-[20px]">
+        <template v-for="(item, key) in props.payments" :key="item">
+          <div class="flex-grow-1 flex-shrink-0">
+            <div class="flex-start">
+              <div class="w-[4px] h-[10px] bg-gradient-linear-[180deg,#1A6BEB,#6EA6FF] rounded-[2px]" />
+              <div class="color-[#333] dark:color-[#fff] ml-[4px] text-[12px]  line-height-[24px]">
+                {{ key }}
+              </div>
+            </div>
+            <div class="color-[#3971F3] dark:color-[#fff] font-semibold  line-height-[24px]">
+              <span class="text-[16px]">{{ item }}</span>
+            </div>
+          </div>
+        </template>
+      </div>
+      <div class="bg-[#c7dafF] dark:opacity-[0.5] h-[1px] w-full mt-[8px]" />
+      <div class="mt-[12px] flex-between">
+        <div />
+        <div class="color-[#333]  dark:color-[#fff] text-[12px] line-height-[24px] flex" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+::-webkit-scrollbar {
+  /* 隐藏Webkit浏览器的滚动条 */
+  display: none;
+}
+.skew-text {
+  display: flex;
+  align-items: flex-start;
+}
+</style>
