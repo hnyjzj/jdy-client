483b400376ff91450a9f640e92c49705c78caa7a
XieJingao <496929151@qq.com>
2025-11-19 17:42:13 +0800

    feat: 添加列表公共卡片组件

---
+++ components/common/card/list.vue
@@ -0,0 +1,20 @@
+<!-- 列表里的卡片组件 -->
+<template>
+  <div
+    class="px-[16px] mb-4 rounded-2 blur-bgc">
+    <div
+      class="flex justify-between py-[8px] text-color"
+      style="border-bottom:1px solid #E6E6E8;"
+    >
+      <slot name="top" />
+      <slot name="status" />
+    </div>
+    <slot name="info" />
+    <div
+      class="py-2"
+      style="border-top:1px solid #E6E6E8;"
+    >
+      <slot name="bottom" />
+    </div>
+  </div>
+</template>
