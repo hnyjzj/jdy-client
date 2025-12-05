385c721b033291e8bb73492fe98c300073523f15
纯鹿人 <73681241+sampson762@users.noreply.github.com>
2025-12-05 10:13:43 +0800

    style: 修改公共数据表格样式 (#384)

--- components/common/datatable/Index.vue
+++ components/common/datatable/Index.vue
@@ -13,6 +13,10 @@ let scrollX = 0
 for (let i = 0; i < props.columns.length; ++i) {
   scrollX += 120
 }
+const { $colorMode } = useNuxtApp()
+const tdColor = computed(() => {
+  return $colorMode.value === 'light' ? '#1A6DD8' : '#fff'
+})
 </script>
 
 <template>
@@ -21,10 +25,12 @@ for (let i = 0; i < props.columns.length; ++i) {
       <n-data-table
         :loading="props.loading"
         :style="{
-          '--n-item-text-color-active': '#4678B9',
-          '--n-item-border-active': '1px solid #2976EF',
-          '--n-item-text-color-hover': '#333',
-          '--n-loading-color': 'orange',
+          '--n-merged-td-color': $colorMode.value === 'light' ? '#fff' : '#1D2C60',
+          '--n-merged-td-text-color': $colorMode.value === 'light' ? '#1A6DD8' : '#fff',
+          '--n-merged-td-color-hover': $colorMode.value === 'light' ? '#DAEAFF' : '#0050B8',
+          '--n-merged-th-color': $colorMode.value === 'light' ? '#F3F3F3' : '#0F1E52',
+          '--n-merged-border-color': 'rgba(57,113,243,0.0)',
+          '--td-color': tdColor,
         }"
         size="small"
         remote
@@ -50,5 +56,11 @@ for (let i = 0; i < props.columns.length; ++i) {
 }
 :deep(.n-data-table-td) {
   border: none !important;
+}
+:deep(.n-data-table .n-data-table-base-table-header) {
+  border-radius: 8px;
+}
+:deep(.n-data-table-tr:hover .n-data-table-td) {
+  color: var(--td-color);
 }
 </style>
