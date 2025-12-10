7368cc6078b70b7aa1769cf37fcfcbf575edd162
sampson762 <1769301207@qq.com>
2025-12-10 09:25:54 +0800

    style(Toggle): 移除固定宽度和内边距样式

--- components/summary/Toggle.vue
+++ components/summary/Toggle.vue
@@ -28,7 +28,7 @@ watch(toggleChart, (val) => {
 
 <template>
   <div
-    class="w-[80px] text-[14px] font-normal text-[#666666] px-[12px] py-[3px] rounded-[4px] cursor-pointer"
+    class=" text-[14px] font-normal text-[#666666] py-[3px] rounded-[4px] cursor-pointer"
     @click="toggleChart = toggleChart === 'list' ? 'chart' : 'list'">
     <div v-if="toggleChart === 'list'" class="flex gap-[2px]">
       <icon name="i-icon:data-bar-chart" color="#0068FF" :size="16" />
