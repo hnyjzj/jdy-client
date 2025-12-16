0a380617afe68e51ff21cfc7be994807f6261179
XieJingao <496929151@qq.com>
2025-12-16 15:54:48 +0800

    fix: 优化切换按钮布局对齐

--- components/summary/Toggle.vue
+++ components/summary/Toggle.vue
@@ -30,7 +30,7 @@ watch(toggleChart, (val) => {
   <div
     class=" text-[14px] font-normal text-[#666666] py-[3px] rounded-[4px] cursor-pointer"
     @click="toggleChart = toggleChart === 'list' ? 'chart' : 'list'">
-    <div v-if="toggleChart === 'list'" class="flex gap-[6px]">
+    <div v-if="toggleChart === 'list'" class="flex gap-[6px] items-center">
       <icon name="i-icon:data-bar-chart" color="#0068FF" :size="16" />
       <span class="text-[14px] text-color">图表</span>
     </div>
